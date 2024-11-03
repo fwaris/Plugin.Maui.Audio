26336cfb4c033a5b4af824b12da500935ec8a164
Faisal Waris <FaisalWaris@live.com>
2024-11-02 20:39:58 -0400

    audio experimentation windows

--- src/Plugin.Maui.Audio/PcmAudioStream.windows.cs
+++ src/Plugin.Maui.Audio/PcmAudioStream.windows.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Buffers;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq.Expressions;
 using System.Net.Mime;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
 using System.Threading.Channels;
 using Windows.Foundation;
+using Windows.Media.Capture;
+using Windows.Networking.Connectivity;
 using Windows.Storage.Streams;
 
 namespace Plugin.Maui.Audio;
@@ -14,10 +19,13 @@ class PcmAudioStream : IRandomAccessStream
 	ulong lastSeek;
 	ulong lastBytes;
 	bool canWrite = true;
+	IBuffer cache;
+	uint cacheBytesWritten = 0;
 
-	public PcmAudioStream(Channel<byte[]> channel)
+	public PcmAudioStream(Channel<byte[]> channel, uint bufferSize = 4096*2*2)
 	{
 		this.channel = channel;
+		cache = new Windows.Storage.Streams.Buffer(bufferSize);
 	}
 	public bool CanRead => true;
 
@@ -29,7 +37,8 @@ class PcmAudioStream : IRandomAccessStream
 
 	public IRandomAccessStream CloneStream()
 	{
-		return this;
+		Debug.WriteLine("cloned pcm stream");
+		return new PcmAudioStream(this.channel,this.cache.Capacity);
 	}
 	public void Dispose()
 	{
@@ -68,21 +77,76 @@ class PcmAudioStream : IRandomAccessStream
 	{
 		return AsyncInfo.FromResult(true);
 	}
+	/// <summary>
+	/// Copy a single data item from the channel into the buffer.
+	/// If the buffer is not large enough, the remaining data is stored in the cache.
+	/// </summary>
+	/// <param name="buffer"></param>
+	/// <param name="count"></param>
+	/// <param name="offset"></param>
+	/// <returns></returns>
+	async Task<uint> CopyFromChannelIntoBuffer(IBuffer buffer, uint count, uint offset)
+	{		
+		var data = await channel.Reader.ReadAsync();
+		uint dataSz = (uint)data.Length;
+		uint cpSz = Math.Min(dataSz, count);
+		int rem = (int)dataSz - (int)cpSz;
+		data.CopyTo(0,buffer,offset, (int)cpSz);
+		if (rem > 0)
+		{
+			data.CopyTo((int)cpSz, cache, 0u, rem);
+			cache.Length = (uint)rem;
+			cacheBytesWritten = 0;
+		}
+		return cpSz;
+	}
+
+	/// <summary>
+	/// Copy the remaining data in the cache to the buffer.
+	/// When all data is copied, the cache is cleared.
+	/// </summary>
+	/// <param name="buffer"></param>
+	/// <param name="count"></param>
+	/// <returns></returns>
+	uint CopyCacheToBuffer(IBuffer buffer, uint count)
+	{
+		uint cpSz = Math.Min(cache.Length - cacheBytesWritten, count);
+		cache.CopyTo(cacheBytesWritten, buffer, 0u, cpSz);
+		cacheBytesWritten += cpSz;
+		if (cacheBytesWritten == cache.Length)
+		{
+			cacheBytesWritten = 0;
+			cache.Length = 0;
+		}
+		return cpSz;	
+	}
+
 	IAsyncOperationWithProgress<IBuffer, uint> IInputStream.ReadAsync(IBuffer buffer, uint count, InputStreamOptions options)
 	{
 		return AsyncInfo.Run<IBuffer, uint>(async (cancellationToken, progress) =>
 		{
 			if (cancellationToken.IsCancellationRequested)
 			{
-				return new byte[0].AsBuffer();
-			}			
-			byte[] data = await channel.Reader.ReadAsync(cancellationToken);
-			this.lastSeek += (ulong)data.Length;
-			Debug.WriteLine($"read {data.Length}");
-			progress.Report((uint)this.lastSeek);
-			return data.AsBuffer();
+				Debug.WriteLine($"pcm read cancelled");
+				return buffer;
+			}
+			uint bytesWritten = 0;
+			if (cache.Length > 0)
+			{
+				bytesWritten += CopyCacheToBuffer(buffer, count);
+				Debug.WriteLine($"pcm read cache {bytesWritten}");
+			}
+			while (bytesWritten < count)
+			{
+				bytesWritten += await CopyFromChannelIntoBuffer(buffer, count - bytesWritten, bytesWritten);
+				Debug.WriteLine($"pcm read channel {bytesWritten} count: {count} cap:{buffer.Capacity}");
+			}
+			progress.Report(bytesWritten);
+			Debug.WriteLine($"pcm read done {bytesWritten} count: {count} cap:{buffer.Capacity}");
+			return buffer;
 		});
 	}
+
 	IAsyncOperationWithProgress<uint, uint> IOutputStream.WriteAsync(IBuffer buffer)
 	{
 		return AsyncInfo.Run<uint, uint>((cancellationToken, progress) => WriteBufferAsync(buffer, progress, cancellationToken));
